22d9b68fd52ac6dc1bdfe9a5ca78e41afa8e4668
BZIvanov <ivanov.biser@gmx.com>
2020-04-17 21:25:25 +0300

    update

--- 07.ReduxAdvanced/src/index.js
+++ 07.ReduxAdvanced/src/index.js
@@ -13,29 +13,34 @@ import resultReducer from './store/reducers/result';
 // here is how we combine more than one reducers
 const rootReducer = combineReducers({
   ctr: counterReducer,
-  res: resultReducer
+  res: resultReducer,
 });
 
 // simple example of middleware. We get all the functions ready from redux we dont call them
-const logger = store => {
-  return next => {
-    return action => {
+const logger = (store) => {
+  return (next) => {
+    return (action) => {
       console.log('[Middleware] Dispatching', action);
       const result = next(action);
       console.log('[Middleware] next state', store.getState());
       return result;
-    }
-  }
+    };
+  };
 };
 
 // composeEnhancers is needed for our google chrome redux extension
+// here we want to use custom enhancer with Chrome and if we dont have Chrome extension installed we will fall back for the basic compose provided by redux
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 // logger is our custom middleware and thunk is installed one used for handling async code
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger, thunk)));
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(logger, thunk))
+);
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-document.getElementById('root'));
+  document.getElementById('root')
+);
