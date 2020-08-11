efcf35b148743d1ea4654f8911e15527d3d157f3
BZIvanov <biser.z.ivanov@gmail.com>
2020-08-11 23:13:24 +0300

    component names starting with capital letter

--- BurgerProject/src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ BurgerProject/src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import classes from './BurgerIngredient.css';
 
-const burgerIngredient = props => {
+const BurgerIngredient = (props) => {
   let ingredient = null;
 
   switch (props.type) {
@@ -37,8 +36,8 @@ const burgerIngredient = props => {
   return ingredient;
 };
 
-burgerIngredient.propTypes = {
-  type: PropTypes.string.isRequired
+BurgerIngredient.propTypes = {
+  type: PropTypes.string.isRequired,
 };
 
-export default burgerIngredient;
+export default BurgerIngredient;
