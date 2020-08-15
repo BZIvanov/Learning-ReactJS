dce50dc457ad6f6e89442b6ee70a5b9d14c042de
BZIvanov <biser.z.ivanov@gmail.com>
2020-08-15 21:29:15 +0300

    books demo classes project

---
+++ Classes/01.BaseSyntax/demo-c/src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Booklist from './Booklist';
+import './App.css';
+
+const App = () => <Booklist />;
+
+export default App;
---
+++ Classes/01.BaseSyntax/demo-c/src/Booklist.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Book from './Book';
+import booksData from './bookData';
+
+export default class Booklist extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: booksData,
+    };
+  }
+
+  handleDelete = (id) => {
+    const filteredBooks = this.state.books.filter((item) => item.id !== id);
+    this.setState({
+      books: filteredBooks,
+    });
+  };
+
+  render() {
+    return (
+      <section>
+        <h3>This is our Booklist</h3>
+        {this.state.books.map((item) => (
+          <Book key={item.id} info={item} handleDelete={this.handleDelete} />
+        ))}
+      </section>
+    );
+  }
+}
