0af97eb1f7d41d5d3595c24825fb2bbe4d651f34
BZIvanov <biser.z.ivanov@gmail.com>
2020-10-28 17:32:37 +0200

    moved redux thunk example in respective folder

--- Hooks/04.Redux/02.Asynchronous/src/components/CounterOutput.js
+++ Hooks/04.Redux/02.Asynchronous/src/components/CounterOutput.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const CounterOutput = (props) => (
-  <div className="CounterOutput">Current Counter: {props.value}</div>
+  <div className='CounterOutput'>Current Counter: {props.value}</div>
 );
 
 export default CounterOutput;
