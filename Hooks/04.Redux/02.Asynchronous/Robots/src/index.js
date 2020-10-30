bdf3974f8ddb4db73f2b94f132aadc09ada4bedf
BZIvanov <biser.z.ivanov@gmail.com>
2020-10-30 18:14:12 +0200

    redux thunk demo project

---
+++ Hooks/04.Redux/02.Asynchronous/Robots/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import 'tachyons';
+import './index.css';
+import App from './containers/App';
+import { requestRobots, searchRobots } from './store/reducers';
+
+const logger = createLogger();
+
+const rootReducers = combineReducers({ requestRobots, searchRobots });
+
+const store = createStore(
+  rootReducers,
+  applyMiddleware(thunkMiddleware, logger)
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
