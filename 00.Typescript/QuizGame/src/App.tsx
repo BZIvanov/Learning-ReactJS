e59447ce6d51e91c0753667b2166a4b1a1506ccb
BZIvanov <ivanov.biser@gmx.com>
2020-11-03 17:57:20 +0200

    quiz game state

--- 00.Typescript/QuizGame/src/App.tsx
+++ 00.Typescript/QuizGame/src/App.tsx
@@ -1,21 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { fetchQuizQuestions } from './API';
-import { Difficulty } from './API';
+import { QuestionsState, Difficulty } from './API';
 
 const TOTAL_QUESTIONS = 10;
 
+export type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+};
+
 const App: React.FC = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState<QuestionsState[]>([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
   const startTrivia = async () => {
+    setLoading(true);
+    setGameOver(false);
     const newQuestions = await fetchQuizQuestions(
       TOTAL_QUESTIONS,
       Difficulty.EASY
     );
-    console.log(newQuestions);
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false);
   };
 
-  startTrivia();
-
-  return <div>Quiz game</div>;
+  return (
+    <div>
+      <h1>REACT QUIZ</h1>
+      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+        <button className='start' onClick={startTrivia}>
+          Start
+        </button>
+      ) : null}
+      {!gameOver ? <p className='score'>Score: {score}</p> : null}
+      {loading ? <p>Loading Questions...</p> : null}
+      {!loading && !gameOver && <div>Question card placeholder</div>}
+      {!gameOver &&
+      !loading &&
+      userAnswers.length === number + 1 &&
+      number !== TOTAL_QUESTIONS - 1 ? (
+        <button className='next'>Next Question</button>
+      ) : null}
+    </div>
+  );
 };
 
 export default App;
