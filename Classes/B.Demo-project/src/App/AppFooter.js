4df34a07dbe291dedfca6fff22f139c17b0fab51
BZIvanov <ivanov.biser@gmx.com>
2021-03-20 19:40:53 +0200

    classes demos

---
+++ Classes/B.Demo-project/src/App/AppFooter.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import SimpleSnackbar from './Snackbar';
+
+const AppFooter = (props) => (
+  <SimpleSnackbar showSnack={props.showSnack} message={props.message} />
+);
+
+export default AppFooter;
