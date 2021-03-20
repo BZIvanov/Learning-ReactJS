5abaa269ac3b4f0914b1d46ec0bdc1d224b860f2
BZIvanov <biser.z.ivanov@gmail.com>
2021-03-20 19:40:53 +0200

    classes demos

---
+++ Classes/B.Demo-project/server/models/Game.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require('mongoose');
+
+const gameSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = model('Game', gameSchema);
