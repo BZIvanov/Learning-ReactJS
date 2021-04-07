14da6647a6b2ccee1ca61ccecfcd11218d8ebad8
BZIvanov <ivanov.biser@gmx.com>
2021-04-07 21:59:43 +0300

    books demo project

---
+++ Classes/C.Demo-project/books/src/views/create-book.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CreateBook extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Create book</h2>
+      </div>
+    );
+  }
+}
+
+export default CreateBook;
---
+++ Classes/C.Demo-project/server/config/database.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const User = require('../models/User');
+
+module.exports = (settings) => {
+  mongoose.connect(settings.db);
+  let db = mongoose.connection;
+
+  db.once('open', (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log('MongoDB ready!');
+    User.seedAdminUser();
+  });
+  db.on('error', (err) => console.log(`Database error: ${err}`));
+};
