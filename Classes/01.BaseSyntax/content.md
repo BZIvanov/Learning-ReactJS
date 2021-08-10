f42a2b35e1901ddcd4c935c9ba39d518b4c30cad
BZIvanov <ivanov.biser@gmx.com>
2021-08-10 22:15:37 +0300

    refactor class basic demo b and c

---
+++ Classes/01.BaseSyntax/content.md
@@ -0,0 +1,23 @@
+## Content
+
+This file contains info of what is covered in the deferent demos.
+
+#### demo-a
+
+- importing css file
+- inline styles
+- styles as separate object
+- children
+- onChange event
+
+#### demo-b
+
+- provide props to child
+- props destructuring
+- render list of items
+
+#### demo-c
+
+- react fragment
+- self closing html element
+- _key_ key word
--- Classes/01.BaseSyntax/demo-b/src/Validation.js
+++ Classes/01.BaseSyntax/demo-b/src/Validation.js
@@ -2,11 +2,8 @@ import React from 'react';
 
 // here on props we will have only inputLength so it is easier to destructure it
 const Validation = ({ inputLength }) => {
-  let validationMessage = 'Text long enough';
-
-  if (inputLength <= 5) {
-    validationMessage = 'Text too short';
-  }
+  const validationMessage =
+    inputLength > 5 ? 'Text long enough' : 'Text too short';
 
   return <p>{validationMessage}</p>;
 };
