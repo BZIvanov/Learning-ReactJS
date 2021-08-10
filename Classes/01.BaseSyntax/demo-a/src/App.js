e184e9ddd34dec28b353d982d4bcafb9ad4a2c87
BZIvanov <biser.z.ivanov@gmail.com>
2021-08-10 21:44:53 +0300

    refactor class basic demo a

--- Classes/01.BaseSyntax/demo-a/src/App.js
+++ Classes/01.BaseSyntax/demo-a/src/App.js
@@ -1,35 +1,44 @@
 import React, { Component } from 'react';
-
 // import components with capital letter
-import UserInput from './UserInput/UserInput';
-import UserOutput from './UserOutput/UserOutput';
+import UserInput from './UserInput';
+import UserOutput from './UserOutput';
 
 class App extends Component {
   // everytime the state is changed the component will rerender
   state = {
-    username: 'supermax',
+    username: '',
     someOtherProp: [],
   };
 
   // with arrow function we dont need to bind this, we will keep it
-  usernameChangedHandler = (event) => {
+  handleInputChange = (event) => {
     this.setState({ username: event.target.value });
   };
 
   // render method is required for class components, because it will return the html we want to be displayed
   render() {
     return (
-      <div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          margin: '20px',
+        }}
+      >
         {/* React components should always start with capital letter */}
         <UserInput
-          changed={this.usernameChangedHandler}
+          valueChange={this.handleInputChange}
           currentName={this.state.username}
         />
         <UserOutput userName={this.state.username} />
         <UserOutput userName={this.state.username} />
-        <UserOutput userName="Ivan" />
+        <UserOutput userName='Ivan' />
         <UserOutput>
-          Content between component tags goes as props.children in the component
+          <p>
+            Content between component tags goes as props.children in the
+            component
+          </p>
         </UserOutput>
       </div>
     );
