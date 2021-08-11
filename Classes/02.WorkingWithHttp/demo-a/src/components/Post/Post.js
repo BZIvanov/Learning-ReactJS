5c655736c2e3c1e9c7daf82eff7ebc4789f26afe
BZIvanov <ivanov.biser@gmx.com>
2021-08-11 07:08:36 +0300

    refactor class http demo a

--- Classes/02.WorkingWithHttp/demo-a/src/components/Post/Post.js
+++ Classes/02.WorkingWithHttp/demo-a/src/components/Post/Post.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './Post.css';
 
 const post = (props) => (
-  <article className="Post" onClick={props.clicked}>
-    <h1>{props.title}</h1>
-    <div className="Info">
-      <div className="Author">{props.author}</div>
+  <article className='Post' onClick={props.postClick}>
+    <h3>{props.title}</h3>
+    <div className='Info'>
+      <div className='Author'>{props.author}</div>
     </div>
   </article>
 );
--- Classes/02.WorkingWithHttp/demo-a/src/index.js
+++ Classes/02.WorkingWithHttp/demo-a/src/index.js
@@ -1,42 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import axios from 'axios';
 
-// AXIOS DEFAULT SETTINGS
-// with the below row we can set default baseUrl and then when making requests we dont need to specify it on each request
-axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
-// and this is how we set some headers for all requests
-axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
-// and this is how we set something for POST requests only for example
-axios.defaults.headers.post['Content-Type'] = 'application/json';
-
-// INTERCEPTORS
-// the interceptor will be used for all the requests for the application
-axios.interceptors.request.use(
-  (request) => {
-    console.log(request);
-    // Edit request config or whatever you want here
-
-    // it is important to return the request so it can continue, otherwise it will be blocked
-    return request;
-  },
-  (error) => {
-    console.log(error);
-    return Promise.reject(error);
-  }
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-axios.interceptors.response.use(
-  (response) => {
-    console.log(response);
-    // Edit response config
-    return response;
-  },
-  (error) => {
-    console.log(error);
-    return Promise.reject(error);
-  }
-);
-
-ReactDOM.render(<App />, document.getElementById('root'));
