97c63f7426b761b7d713a1218aec8a2b1f9e5677
BZIvanov <biser.z.ivanov@gmail.com>
2021-08-17 22:24:08 +0300

    redux with ts demo counter

---
+++ Typescript/demos/redux-count/src/store/action-creators/index.ts
@@ -0,0 +1,29 @@
+import { Dispatch } from 'redux';
+import { ActionType } from '../action-types';
+import { Action } from '../actions';
+
+export const increaseAmount = (amount: number) => {
+  return (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.INCREASE,
+      payload: amount,
+    });
+  };
+};
+
+export const decreaseAmount = (amount: number) => {
+  return (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.DECREASE,
+      payload: amount,
+    });
+  };
+};
+
+export const resetAmount = () => {
+  return (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.RESET,
+    });
+  };
+};
