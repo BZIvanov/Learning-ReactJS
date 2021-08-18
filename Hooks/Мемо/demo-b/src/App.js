0402d6873956f24c1b25b238493222e6c44c2b78
BZIvanov <ivanov.biser@gmx.com>
2021-08-18 22:59:46 +0300

    useMemo hook example

---
+++ Hooks/Мемо/demo-b/src/App.js
@@ -0,0 +1,17 @@
+import BadExample from './BadExample';
+import GoodExample from './GoodExample';
+import GoodExampleA from './GoodExampleA';
+
+const App = () => {
+  return (
+    <div>
+      <BadExample />
+      <hr />
+      <GoodExample />
+      <hr />
+      <GoodExampleA />
+    </div>
+  );
+};
+
+export default App;
