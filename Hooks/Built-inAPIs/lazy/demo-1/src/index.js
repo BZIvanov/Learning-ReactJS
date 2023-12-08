e06f184ffec5870176e8b1ed414998e14cc94575
BZIvanov <biser.z.ivanov@gmail.com>
2023-12-08 15:41:21 +0200

    create separate folder for react apis

--- Hooks/Built-inAPIs/lazy/demo-1/src/index.js
+++ Hooks/Built-inAPIs/lazy/demo-1/src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'; // 17.0.1
+import ReactDOM from 'react-dom'; // 17.0.1
 import App from './App';
 
 ReactDOM.render(
--- Hooks/Мемо/demo-c/src/Step.js
+++
@@ -1,5 +0,0 @@
-const Footer = ({ change }) => {
-  return <div>{change ? 'x5' : 'not x5'}</div>;
-};
-
-export default Footer;
