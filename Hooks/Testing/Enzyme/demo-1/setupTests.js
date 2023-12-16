9ed9deff16f8e812f24aefdd790756ddb21c0dc8
BZIvanov <ivanov.biser@gmx.com>
2023-12-16 14:35:04 +0200

    update folder structure

--- Hooks/Testing/Enzyme/demo-1/setupTests.js
+++ Hooks/Testing/Enzyme/demo-1/setupTests.js
@@ -3,7 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-import Enzyme from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import Enzyme from 'enzyme'; // 3.11.0
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'; // 0.6.5
 
 Enzyme.configure({ adapter: new Adapter() });
