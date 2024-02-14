da0fca9380fe91666250a922a862dd1e8e35e566
BZIvanov <ivanov.biser@gmx.com>
2024-02-14 21:05:32 +0200

    lighten and darken methods examples

---
+++ Hooks/StylingLibraries/MaterialUI/modify-color/App.jsx
@@ -0,0 +1,31 @@
+import { Box, Typography, lighten, darken } from '@mui/material';
+
+const App = () => {
+  // with lighten and darken method we can provide some color and then percantage with which to modify the color
+
+  return (
+    <Box>
+      <Typography
+        sx={{
+          backgroundColor: (theme) => lighten(theme.palette.primary.main, 0.5),
+        }}
+      >
+        Text
+      </Typography>
+      <Typography
+        sx={{ backgroundColor: (theme) => theme.palette.primary.main }}
+      >
+        Text
+      </Typography>
+      <Typography
+        sx={{
+          backgroundColor: (theme) => darken(theme.palette.primary.main, 0.3),
+        }}
+      >
+        Text
+      </Typography>
+    </Box>
+  );
+};
+
+export default App;
